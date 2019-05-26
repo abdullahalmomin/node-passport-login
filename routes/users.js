3b3a6cf99987594ef8d5e6b7d298d2388d8a1ff6
abdullahalmomin <abd.momin003@gmail.com>
2019-05-26 15:36:40 +0600

    Registration complete

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// User model
+const User = require("../models/User");
 
 // Login page
 router.get("/login", (req, res) => res.render("login"));
@@ -32,7 +36,49 @@ router.post("/register", (req, res) => {
       password2
     });
   } else {
-    res.send("pass");
+    // res.send("pass");
+    //  Validation passed
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: "email is already registered" });
+        res.render("register", {
+          // user exist
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        //Hash Password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            //set password to hash
+            newUser.password = hash;
+            //save user
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  "success_msg",
+                  "You are now registered and can log in"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(err => console.log(err));
+          });
+        });
+        // console.log(newUser);
+        // res.send("hello");
+      }
+    });
   }
 });
 module.exports = router;
